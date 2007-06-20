d39a249310dbb822825f2888504d5d353b67a925
annie_zhou91 <annie_zhou91>
2007-06-20 03:20:49 +0000

    *** empty log message ***

--- web/WEB-INF/classes/src/org/oscarehr/PMmodule/service/impl/AdmissionManagerImpl.java
+++ web/WEB-INF/classes/src/org/oscarehr/PMmodule/service/impl/AdmissionManagerImpl.java
@@ -70,8 +70,8 @@ public class AdmissionManagerImpl implements AdmissionManager {
 	    this.bedDemographicManager = bedDemographicManager;
     }
 	
-	public Admission getAdmission_archiveView(String programId, Integer demographicNo) {
-		return dao.getAdmission(Integer.valueOf(programId), demographicNo);
+	public List getAdmissions_archiveView(String programId, Integer demographicNo) {
+		return dao.getAdmissions_archiveView(Integer.valueOf(programId), demographicNo);
 	}
 	
 	public Admission getAdmission(String programId, Integer demographicNo) {

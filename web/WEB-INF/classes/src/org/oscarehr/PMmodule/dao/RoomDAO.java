2ad80a2a7608dee4a4710721548465c4ea441435
opiate <opiate>
2007-08-28 22:17:08 +0000

    Refactoring; removing unnecessary interfaces for bed manager, room dao, and bed dao.

--- web/WEB-INF/classes/src/org/oscarehr/PMmodule/dao/RoomDAO.java
+++ web/WEB-INF/classes/src/org/oscarehr/PMmodule/dao/RoomDAO.java
@@ -22,69 +22,171 @@
 
 package org.oscarehr.PMmodule.dao;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.oscarehr.PMmodule.model.Room;
 import org.oscarehr.PMmodule.model.RoomType;
+import org.springframework.orm.hibernate3.support.HibernateDaoSupport;
 
-public interface RoomDAO {
+/**
+ * Hibernate implementation of RoomDAO
+ */
+public class RoomDAO extends HibernateDaoSupport {
+
+	private static final Log log = LogFactory.getLog(RoomDAO.class);
 
 	/**
 	 * Does room with id exist
-	 * 
+	 *
 	 * @param roomId
 	 *            id
 	 * @return true if room exists
 	 */
-	public boolean roomExists(Integer roomId);
+    public boolean roomExists(Integer roomId) {
+		boolean exists = (((Integer) getHibernateTemplate().iterate("select count(*) from Room where id = " + roomId).next()) == 1);
+		log.debug("roomExists: " + exists);
+
+		return exists;
+	}
+
 
 	/**
 	 * Does room type with id exist
-	 * 
+	 *
 	 * @param roomTypeId
 	 *            id
 	 * @return true if room type exists
 	 */
-	public boolean roomTypeExists(Integer roomTypeId);
+    public boolean roomTypeExists(Integer roomTypeId) {
+		boolean exists = (((Integer) getHibernateTemplate().iterate("select count(*) from RoomType where id = " + roomTypeId).next()) == 1);
+		log.debug("roomTypeExists: " + exists);
+
+		return exists;
+	}
 
 	/**
 	 * Get room by id
-	 * 
+	 *
 	 * @param roomId
 	 *            id
 	 * @return room
 	 */
-	public Room getRoom(Integer roomId);
+    public Room getRoom(Integer roomId) {
+		Room room = (Room) getHibernateTemplate().get(Room.class, roomId);
+		log.debug("getRoom: id: " + roomId);
+
+		return room;
+	}
+
 
 	/**
 	 * Get room type by id
-	 * 
+	 *
 	 * @param roomTypeId
 	 *            id
 	 * @return room type
 	 */
-	public RoomType getRoomType(Integer roomTypeId);
+    public RoomType getRoomType(Integer roomTypeId) {
+		RoomType roomType = (RoomType) getHibernateTemplate().get(RoomType.class, roomTypeId);
+		log.debug("getRoom: id: " + roomTypeId);
+
+		return roomType;
+	}
 
 	/**
 	 * Get rooms
-	 * 
+	 *
 	 * @param active
 	 *            filter
 	 * @return list of rooms
 	 */
-	public Room[] getRooms(Integer programId, Boolean active);
+    @SuppressWarnings("unchecked")
+    public Room[] getRooms(Integer programId, Boolean active) {
+		String queryString = getRoomsQueryString(programId, active);
+		Object[] values = getRoomsValues(programId, active);
+
+		List rooms = (programId != null || active != null) ? getHibernateTemplate().find(queryString, values) : getHibernateTemplate().find(queryString);
+		log.debug("getRooms: size: " + rooms.size());
+
+		return (Room[]) rooms.toArray(new Room[rooms.size()]);
+	}
+
 
 	/**
 	 * Get room types
-	 * 
+	 *
 	 * @return
 	 */
-	public RoomType[] getRoomTypes();
+    @SuppressWarnings("unchecked")
+    public RoomType[] getRoomTypes() {
+		List roomTypes = getHibernateTemplate().find("from RoomType rt");
+		log.debug("getRooms: size: " + roomTypes.size());
+
+		return (RoomType[]) roomTypes.toArray(new RoomType[roomTypes.size()]);
+	}
+
 
 	/**
 	 * Save room
-	 * 
+	 *
 	 * @param room
 	 *            room to save
 	 */
-	public void saveRoom(Room room);
+    public void saveRoom(Room room) {
+		updateHistory(room);
+		getHibernateTemplate().saveOrUpdate(room);
+		getHibernateTemplate().flush();
+		getHibernateTemplate().refresh(room);
+		
+		log.debug("saveRoom: id: " + room.getId());
+	}
+
+	String getRoomsQueryString(Integer programId, Boolean active) {
+		StringBuilder queryBuilder = new StringBuilder("from Room r");
+
+		if (programId != null || active != null) {
+			queryBuilder.append(" where ");
+		}
+
+		if (programId != null) {
+			queryBuilder.append("r.programId = ?");
+		}
+
+		if (programId != null && active != null) {
+			queryBuilder.append(" and ");
+		}
+
+		if (active != null) {
+			queryBuilder.append("r.active = ?");
+		}
+
+		return queryBuilder.toString();
+	}
+	
+	Object[] getRoomsValues(Integer programId, Boolean active) {
+		List<Object> values = new ArrayList<Object>();
+		
+		if (programId != null) {
+			values.add(programId);
+		}
+		
+		if (active != null) {
+			values.add(active);
+		}
+		
+		return (Object[]) values.toArray(new Object[values.size()]);
+	}
+
+	void updateHistory(Room room) {
+		// TODO IC Bedlog Historical - update create and persist historical data
+		// get previous programroom
+		// set end date to today
+		// create new programroom
+		// set start date to today
+		// save previous and new programrooms
+	}
 
 }

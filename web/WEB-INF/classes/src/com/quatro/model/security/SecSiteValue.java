909a036c62aab71664cbecba7761b61f3d5c1fbc
tonywzt1 <tonywzt1>
2009-06-11 21:54:21 +0000

    added missing SerialVersionUID

--- web/WEB-INF/classes/src/com/quatro/model/security/SecSiteValue.java
+++ web/WEB-INF/classes/src/com/quatro/model/security/SecSiteValue.java
@@ -13,6 +13,10 @@ import java.io.Serializable;
 import java.util.Date;
 
 public class SecSiteValue implements Serializable {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 	String siteId;
 	int siteKey;
 	String userName;

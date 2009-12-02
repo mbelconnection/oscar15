f620644adc436958e5161ce062b3936a4a7398a1
tedleung <tedleung>
2009-12-02 20:55:25 +0000

    fix for showing the wrong version of a note

--- web/WEB-INF/classes/src/org/oscarehr/casemgmt/dao/CaseManagementNoteDAO.java
+++ web/WEB-INF/classes/src/org/oscarehr/casemgmt/dao/CaseManagementNoteDAO.java
@@ -256,13 +256,13 @@ public class CaseManagementNoteDAO extends HibernateDaoSupport {
 		{
 			session.close();
 		}
-		
+
 		// make unique for uuid
 		HashMap<String,CaseManagementNote> uniqueForUuid=new HashMap<String,CaseManagementNote>();
 		for (CaseManagementNote note : notes)
 		{
 			CaseManagementNote existingNote=uniqueForUuid.get(note.getUuid());
-			if (existingNote==null || note.getObservation_date().after(existingNote.getObservation_date())) uniqueForUuid.put(note.getUuid(), note);
+			if (existingNote==null || note.getUpdate_date().after(existingNote.getUpdate_date())) uniqueForUuid.put(note.getUuid(), note);
 		}
 		
 		// sort by observationdate

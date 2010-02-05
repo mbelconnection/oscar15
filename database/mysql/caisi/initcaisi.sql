0b2e8f97d0d413f0d3a78173c128c676108309b0
annie_zhou91 <annie_zhou91>
2010-02-05 15:03:37 +0000

    added the column label to intake_answer_element

--- database/mysql/caisi/initcaisi.sql
+++ database/mysql/caisi/initcaisi.sql
@@ -1508,6 +1508,7 @@ CREATE TABLE `intake_answer_element` (
   `intake_answer_validation_id` int(10) unsigned default NULL,
   `dflt` tinyint(1) default '0',
   `element` varchar(255) NOT NULL,
+  `label` varchar(255),
   PRIMARY KEY  (`intake_answer_element_id`),
   KEY `IDX_intake_answer_element_intake_node_template` (`intake_node_template_id`),
   KEY `IDX_intake_answer_element_intake_answer_validation` (`intake_answer_validation_id`),
@@ -2606,18 +2607,5 @@ create table CdsClientFormData
 	question varchar(64) not null,
 	index(question),
 	answer varchar(16) not null
-);
-
-DROP TABLE IF EXISTS `oncall_questionnaire`;
-CREATE TABLE `oncall_questionnaire` (
-        `id` int(10) unsigned NOT NULL auto_increment,
-        `providerNo` varchar(40) NOT NULL,
-        `type` varchar(250) NOT NULL,
-        `health_type` varchar(50) NOT NULL,
-        `nurse_involved` varchar(50) NOT NULL,
-        `course_of_action` varchar(50) NOT NULL,
-        `physician_consultation_reqd` varchar(50) NOT NULL,
-        `call_time` datetime NOT NULL,
-        PRIMARY KEY  (`id`)
 );
 

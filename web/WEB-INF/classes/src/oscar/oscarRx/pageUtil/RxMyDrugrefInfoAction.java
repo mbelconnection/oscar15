3db474e39fa0d6fe8e7a732160de3cad7a262949
ygmgbg <ygmgbg>
2010-03-04 16:26:16 +0000

    -add a method to handle current interacting drugs -catch any exceptions with mydrugref info

--- web/WEB-INF/classes/src/oscar/oscarRx/pageUtil/RxMyDrugrefInfoAction.java
+++ web/WEB-INF/classes/src/oscar/oscarRx/pageUtil/RxMyDrugrefInfoAction.java
@@ -45,151 +45,183 @@ import org.oscarehr.common.model.*;
 import oscar.OscarProperties;
 import oscar.oscarRx.util.TimingOutCallback;
 import oscar.oscarRx.util.TimingOutCallback.TimeoutException;
-
+import oscar.oscarRx.util.RxUtil;
 
 public final class RxMyDrugrefInfoAction extends DispatchAction {
 
     private static Log log2 = LogFactory.getLog(RxMyDrugrefInfoAction.class);
-    
+    //return interactions about current pending prescriptions
+    public ActionForward findInteractingDrugList (ActionMapping mapping,ActionForm form,HttpServletRequest request,HttpServletResponse response)throws IOException, ServletException {
+        System.out.println("in findInteractingDrugList");
+         oscar.oscarRx.pageUtil.RxSessionBean bean = (oscar.oscarRx.pageUtil.RxSessionBean) request.getSession().getAttribute("RxSessionBean");
+         if (bean == null) {
+                response.sendRedirect("error.html");
+                return null;
+            }
+      try{
+            WebApplicationContext ctx = WebApplicationContextUtils.getRequiredWebApplicationContext(getServlet().getServletContext());
+            UserPropertyDAO  propDAO =  (UserPropertyDAO) ctx.getBean("UserPropertyDAO");
+            String provider = (String) request.getSession().getAttribute("user");
+            String retStr=RxUtil.findInterDrugStr(propDAO,provider,bean);
+            //System.out.println("retStr="+retStr);
+            bean.setInteractingDrugList(retStr);
+          /*  int pp=23;
+            if(pp==23)
+                throw new Exception();*/
+     }catch(Exception e){
+        e.printStackTrace();
+        ResourceBundle prop = ResourceBundle.getBundle("oscarResources");
+        String failedMsg=prop.getString("oscarRx.MyDrugref.InteractingDrugs.error.msgFailed");
+        bean.setInteractingDrugList(failedMsg);            
+     }
+        return null;
+    }
+
     public ActionForward view(ActionMapping mapping,ActionForm form,HttpServletRequest request,HttpServletResponse response)throws IOException, ServletException {
         System.out.println("in view RxMyDrugrefInfoAction");
-        long start = System.currentTimeMillis();
-        String target=(String)request.getParameter("target");
-        if(target==null) System.out.println("target is null");
-        else if(target.equals("interactionsRx")) System.out.println("target is interactionsRx");
-        String provider = (String) request.getSession().getAttribute("user");
-        
-        WebApplicationContext ctx = WebApplicationContextUtils.getRequiredWebApplicationContext(getServlet().getServletContext());
-        UserPropertyDAO  propDAO =  (UserPropertyDAO) ctx.getBean("UserPropertyDAO");
-        UserDSMessagePrefsDAO  dsmessageDAO =  (UserDSMessagePrefsDAO) ctx.getBean("UserDSMessagePrefsDAO");
-        System.out.println("hideResources is before "+request.getSession().getAttribute("hideResources"));
-        Hashtable dsPrefs=new Hashtable();
-        if (request.getSession().getAttribute("hideResources") == null){
-            //System.out.println("hideResources attribute is null ");
-            dsPrefs = dsmessageDAO.getHashofMessages(provider,UserDSMessagePrefs.MYDRUGREF);
-            //System.out.println("dsPrefs="+dsPrefs);
-        }        
-        UserProperty prop = propDAO.getProp(provider, UserProperty.MYDRUGREF_ID);
-        String myDrugrefId = null;
-        if (prop != null){
-            myDrugrefId = prop.getValue();
-           // System.out.println(myDrugrefId);
-        }
-        
-        RxSessionBean bean = (RxSessionBean) request.getSession().getAttribute("RxSessionBean");
-        if ( bean == null ){
-            return mapping.findForward("success");
-        }
-        Vector codes = bean.getAtcCodes();
-        //String[] str = new String[]{"warnings_byATC","bulletins_byATC","interactions_byATC"};
-        String[] str = new String[]{"warnings_byATC,bulletins_byATC,interactions_byATC,get_guidelines"};   //NEW more efficent way of sending multiple requests at the same time.
-        MessageResources mr=getResources(request);
-        Locale locale = getLocale(request);
-        
-        Vector all = new Vector();
-        for (String command : str){
-            try{
-                Vector v = getMyDrugrefInfo(command,  codes,myDrugrefId) ;
-               // System.out.println("v in for loop: "+v);
-                if (v !=null && v.size() > 0){
-                    all.addAll(v);
-                }
-                //System.out.println("after all.addAll(v): "+all);
-            }catch(Exception e){
-                log2.debug("command :"+command+" "+e.getMessage());
-                e.printStackTrace();
-            }
-        }
-        Collections.sort(all, new MyDrugrefComparator());
-        System.out.println(all);
-        //loop through all to add interaction to each warning
         try{
+
+            long start = System.currentTimeMillis();
+            String target=(String)request.getParameter("target");
+            if(target==null) System.out.println("target is null");
+            else if(target.equals("interactionsRx")) System.out.println("target is interactionsRx");
+            String provider = (String) request.getSession().getAttribute("user");
+
+            WebApplicationContext ctx = WebApplicationContextUtils.getRequiredWebApplicationContext(getServlet().getServletContext());
+            UserPropertyDAO  propDAO =  (UserPropertyDAO) ctx.getBean("UserPropertyDAO");
+            UserDSMessagePrefsDAO  dsmessageDAO =  (UserDSMessagePrefsDAO) ctx.getBean("UserDSMessagePrefsDAO");
+            System.out.println("hideResources is before "+request.getSession().getAttribute("hideResources"));
+            Hashtable dsPrefs=new Hashtable();
+            if (request.getSession().getAttribute("hideResources") == null){
+                //System.out.println("hideResources attribute is null ");
+                dsPrefs = dsmessageDAO.getHashofMessages(provider,UserDSMessagePrefs.MYDRUGREF);
+                //System.out.println("dsPrefs="+dsPrefs);
+            }
+            UserProperty prop = propDAO.getProp(provider, UserProperty.MYDRUGREF_ID);
+            String myDrugrefId = null;
+            if (prop != null){
+                myDrugrefId = prop.getValue();
+               // System.out.println(myDrugrefId);
+            }
+
+            RxSessionBean bean = (RxSessionBean) request.getSession().getAttribute("RxSessionBean");
+            if ( bean == null ){
+                return mapping.findForward("success");
+            }
+            Vector codes = bean.getAtcCodes();
+            //String[] str = new String[]{"warnings_byATC","bulletins_byATC","interactions_byATC"};
+            String[] str = new String[]{"warnings_byATC,bulletins_byATC,interactions_byATC,get_guidelines"};   //NEW more efficent way of sending multiple requests at the same time.
+            MessageResources mr=getResources(request);
+            Locale locale = getLocale(request);
+
+            Vector all = new Vector();
+            for (String command : str){
+                try{
+                    Vector v = getMyDrugrefInfo(command,  codes,myDrugrefId) ;
+                   // System.out.println("v in for loop: "+v);
+                    if (v !=null && v.size() > 0){
+                        all.addAll(v);
+                    }
+                    //System.out.println("after all.addAll(v): "+all);
+                }catch(Exception e){
+                    log2.debug("command :"+command+" "+e.getMessage());
+                    e.printStackTrace();
+                }
+            }
+            Collections.sort(all, new MyDrugrefComparator());
+            System.out.println(all);
+            //loop through all to add interaction to each warning
+            try{
+                for(int i=0;i<all.size();i++){
+                    Hashtable ht=(Hashtable)all.get(i);
+                    System.out.println("**ht="+ht);
+                    String effect=(String)ht.get("effect");
+                    System.out.println("**effect="+effect);
+                    String interactStr="";
+                   if(effect!=null){
+                        if(effect.equals("a"))
+                            effect=mr.getMessage(locale, "oscarRx.interactions.msgAugmentsNoClinical");
+                        else if(effect.equals("A"))
+                            effect=mr.getMessage(locale, "oscarRx.interactions.msgAugments");
+                        else if(effect.equals("i"))
+                            effect=mr.getMessage(locale, "oscarRx.interactions.msgInhibitsNoClinical");
+                        else if(effect.equals("I"))
+                            effect=mr.getMessage(locale, "oscarRx.interactions.msgInhibits");
+                        else if(effect.equals("n"))
+                            effect=mr.getMessage(locale, "oscarRx.interactions.msgNoEffect");
+                        else if(effect.equals("N"))
+                            effect=mr.getMessage(locale, "oscarRx.interactions.msgNoEffect");
+                        else if(effect.equals(" "))
+                            effect=mr.getMessage(locale, "oscarRx.interactions.msgUnknownEffect");
+                        interactStr=ht.get("name")+" "+effect+" "+ht.get("drug2");
+                   }
+                    ht.put("interactStr", interactStr);
+                    System.out.println("ineractStr="+interactStr);
+                }
+            }catch(NullPointerException npe){
+                npe.printStackTrace();
+            }
+            //Vector idWarningVec=new Vector();
+            Vector<Hashtable> allRetVec=new Vector();
+            Vector<String> currentIdWarnings=new Vector();
             for(int i=0;i<all.size();i++){
                 Hashtable ht=(Hashtable)all.get(i);
-                System.out.println("**ht="+ht);
-                String effect=(String)ht.get("effect");
-                System.out.println("**effect="+effect);
-                String interactStr="";
-               if(effect!=null){
-                    if(effect.equals("a"))
-                        effect=mr.getMessage(locale, "oscarRx.interactions.msgAugmentsNoClinical");
-                    else if(effect.equals("A"))
-                        effect=mr.getMessage(locale, "oscarRx.interactions.msgAugments");
-                    else if(effect.equals("i"))
-                        effect=mr.getMessage(locale, "oscarRx.interactions.msgInhibitsNoClinical");
-                    else if(effect.equals("I"))
-                        effect=mr.getMessage(locale, "oscarRx.interactions.msgInhibits");
-                    else if(effect.equals("n"))
-                        effect=mr.getMessage(locale, "oscarRx.interactions.msgNoEffect");
-                    else if(effect.equals("N"))
-                        effect=mr.getMessage(locale, "oscarRx.interactions.msgNoEffect");
-                    else if(effect.equals(" "))
-                        effect=mr.getMessage(locale, "oscarRx.interactions.msgUnknownEffect");
-                    interactStr=ht.get("name")+" "+effect+" "+ht.get("drug2");
-               }
-                ht.put("interactStr", interactStr);
-                System.out.println("ineractStr="+interactStr);
+                Date dt=(Date)ht.get("updated_at");
+                Long time=dt.getTime();
+                String idWarning=ht.get("id")+"."+time;
+                if(!currentIdWarnings.contains(idWarning)){
+                    currentIdWarnings.add(idWarning);
+                    allRetVec.add(ht);
+                    //idWarningVec.add(idWarning);
+                }
             }
-        }catch(NullPointerException npe){
-            npe.printStackTrace();
-        }
-        //Vector idWarningVec=new Vector();
-        Vector<Hashtable> allRetVec=new Vector();
-        Vector<String> currentIdWarnings=new Vector();
-        for(int i=0;i<all.size();i++){
-            Hashtable ht=(Hashtable)all.get(i);
-            Date dt=(Date)ht.get("updated_at");
-            Long time=dt.getTime();
-            String idWarning=ht.get("id")+"."+time;
-            if(!currentIdWarnings.contains(idWarning)){
-                currentIdWarnings.add(idWarning);
-                allRetVec.add(ht);
-                //idWarningVec.add(idWarning);
-            }
-        }
-        System.out.println("currentIdWarnings is  "+currentIdWarnings);
-        //set session attribute hiddenResources if it was null
-        if(dsPrefs!=null && dsPrefs.size()>0){
-            Hashtable hiddenR=new Hashtable();
-            Enumeration em=dsPrefs.keys();
-            while(em.hasMoreElements()){
-                String resId=(String)em.nextElement();
-                resId=resId.replace(UserDSMessagePrefs.MYDRUGREF, "");
-                for(String warning:currentIdWarnings){
-                    if(warning.contains(resId)){
-                        String[] arr=warning.split("\\.");
-                        hiddenR.put(UserDSMessagePrefs.MYDRUGREF+resId, arr[1]);
+            System.out.println("currentIdWarnings is  "+currentIdWarnings);
+            //set session attribute hiddenResources if it was null
+            if(dsPrefs!=null && dsPrefs.size()>0){
+                Hashtable hiddenR=new Hashtable();
+                Enumeration em=dsPrefs.keys();
+                while(em.hasMoreElements()){
+                    String resId=(String)em.nextElement();
+                    resId=resId.replace(UserDSMessagePrefs.MYDRUGREF, "");
+                    for(String warning:currentIdWarnings){
+                        if(warning.contains(resId)){
+                            String[] arr=warning.split("\\.");
+                            hiddenR.put(UserDSMessagePrefs.MYDRUGREF+resId, arr[1]);
+                        }
                     }
                 }
+                request.getSession().setAttribute("hideResources", hiddenR);
             }
-            request.getSession().setAttribute("hideResources", hiddenR);
-        }
-        //if hideResources are not in warnings, remove them from hiddenResource and set them to archived=0 in database;
-        Hashtable hiddenResAttribute=(Hashtable)request.getSession().getAttribute("hideResources");
-        if(hiddenResAttribute==null){
-            Hashtable emptyHiddenRes=new Hashtable();
-            request.getSession().setAttribute("hideResources", emptyHiddenRes);
-        }else{
-            Enumeration hiddenResKeys=hiddenResAttribute.keys();
-            while(hiddenResKeys.hasMoreElements()){
-                String key=(String)hiddenResKeys.nextElement();
-                String value=(String)hiddenResAttribute.get(key);
-                Date updatedatId=new Date();
-                updatedatId.setTime(Long.parseLong(value));
-                String resId=key.replace(UserDSMessagePrefs.MYDRUGREF, "");
-                String id=resId+"."+value;
-                if(!currentIdWarnings.contains(id)){
-                    hiddenResAttribute.remove(key);
-                    //update database
-                    setShowDSMessage(dsmessageDAO, provider, resId, updatedatId);
+            //if hideResources are not in warnings, remove them from hiddenResource and set them to archived=0 in database;
+            Hashtable hiddenResAttribute=(Hashtable)request.getSession().getAttribute("hideResources");
+            if(hiddenResAttribute==null){
+                Hashtable emptyHiddenRes=new Hashtable();
+                request.getSession().setAttribute("hideResources", emptyHiddenRes);
+            }else{
+                Enumeration hiddenResKeys=hiddenResAttribute.keys();
+                while(hiddenResKeys.hasMoreElements()){
+                    String key=(String)hiddenResKeys.nextElement();
+                    String value=(String)hiddenResAttribute.get(key);
+                    Date updatedatId=new Date();
+                    updatedatId.setTime(Long.parseLong(value));
+                    String resId=key.replace(UserDSMessagePrefs.MYDRUGREF, "");
+                    String id=resId+"."+value;
+                    if(!currentIdWarnings.contains(id)){
+                        hiddenResAttribute.remove(key);
+                        //update database
+                        setShowDSMessage(dsmessageDAO, provider, resId, updatedatId);
+                    }
                 }
+                request.getSession().setAttribute("hideResources", hiddenResAttribute);
             }
-            request.getSession().setAttribute("hideResources", hiddenResAttribute);
+            request.setAttribute("warnings",allRetVec);
+            log2.debug("MyDrugref return time " + (System.currentTimeMillis() - start) );
+                if(target!=null && target.equals("interactionsRx")) return mapping.findForward("updateInteractions");
+                else return mapping.findForward("success");
+        }catch(Exception e){
+            e.printStackTrace();
+            return mapping.findForward("failure");
         }
-        request.setAttribute("warnings",allRetVec);
-        log2.debug("MyDrugref return time " + (System.currentTimeMillis() - start) );
-        if(target!=null && target.equals("interactionsRx")) return mapping.findForward("updateInteractions");
-        else return mapping.findForward("success");
     }
     
     private void setShowDSMessage(UserDSMessagePrefsDAO  dsmessageDAO,String provider,String resId,Date updatedatId){

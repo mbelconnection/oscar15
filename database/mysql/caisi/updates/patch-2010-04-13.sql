de12979ce0165163648d1aed400a3e1bae6b13b8
annie_zhou91 <annie_zhou91>
2010-04-14 01:05:25 +0000

    *** empty log message ***

--- database/mysql/caisi/updates/patch-2010-04-13.sql
+++ database/mysql/caisi/updates/patch-2010-04-13.sql
@@ -1,26 +1,19 @@
+
+insert into access_type (name, type) values("read ticklers assigned to a nurse","access");
+
 insert into default_role_access (role_id,access_id) values ((select role_id from caisi_role where name='doctor'),(select access_id from access_type where name='read ticklers assigned to a nurse'));
-insert into default_role_access (role_id,access_id) values ((select role_id from caisi_role where name='doctor'),(select access_id from access_type where name='read nurse issues'));
-insert into default_role_access (role_id,access_id) values ((select role_id from caisi_role where name='doctor'),(select access_id from access_type where name='write nurse issues'));
 insert into default_role_access (role_id,access_id) values ((select role_id from caisi_role where name='doctor'),(select access_id from access_type where name='read nurse notes'));
 
 
 insert into default_role_access (role_id,access_id) values ((select role_id from caisi_role where name='psychiatrist'),(select access_id from access_type where name='read ticklers assigned to a nurse'));
-insert into default_role_access (role_id,access_id) values ((select role_id from caisi_role where name='psychiatrist'),(select access_id from access_type where name='read nurse issues'));
-insert into default_role_access (role_id,access_id) values ((select role_id from caisi_role where name='psychiatrist'),(select access_id from access_type where name='write nurse issues'));
 insert into default_role_access (role_id,access_id) values ((select role_id from caisi_role where name='psychiatrist'),(select access_id from access_type where name='read nurse notes'));
 
 insert into default_role_access (role_id,access_id) values ((select role_id from caisi_role where name='RN'),(select access_id from access_type where name='read ticklers assigned to a nurse'));
-insert into default_role_access (role_id,access_id) values ((select role_id from caisi_role where name='RN'),(select access_id from access_type where name='read nurse issues'));
-insert into default_role_access (role_id,access_id) values ((select role_id from caisi_role where name='RN'),(select access_id from access_type where name='write nurse issues'));
 insert into default_role_access (role_id,access_id) values ((select role_id from caisi_role where name='RN'),(select access_id from access_type where name='read nurse notes'));
 
 insert into default_role_access (role_id,access_id) values ((select role_id from caisi_role where name='RPN'),(select access_id from access_type where name='read ticklers assigned to a nurse'));
-insert into default_role_access (role_id,access_id) values ((select role_id from caisi_role where name='RPN'),(select access_id from access_type where name='read nurse issues'));
-insert into default_role_access (role_id,access_id) values ((select role_id from caisi_role where name='RPN'),(select access_id from access_type where name='write nurse issues'));
 insert into default_role_access (role_id,access_id) values ((select role_id from caisi_role where name='RPN'),(select access_id from access_type where name='read nurse notes'));
 
 insert into default_role_access (role_id,access_id) values ((select role_id from caisi_role where name='Nurse Manager'),(select access_id from access_type where name='read ticklers assigned to a nurse'));
-insert into default_role_access (role_id,access_id) values ((select role_id from caisi_role where name='Nurse Manager'),(select access_id from access_type where name='read nurse issues'));
-insert into default_role_access (role_id,access_id) values ((select role_id from caisi_role where name='Nurse Manager'),(select access_id from access_type where name='write nurse issues'));
 insert into default_role_access (role_id,access_id) values ((select role_id from caisi_role where name='Nurse Manager'),(select access_id from access_type where name='read nurse notes'));
 

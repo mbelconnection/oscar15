2839bcbdb1da07d79b2ee5243d51265dee73ec83
ygmgbg <ygmgbg>
2010-05-17 15:52:52 +0000

    -feature request, id:999381 -add new method to return pharmacy info as json object

--- web/WEB-INF/classes/src/oscar/oscarRx/pageUtil/RxManagePharmacyAction.java
+++ web/WEB-INF/classes/src/oscar/oscarRx/pageUtil/RxManagePharmacyAction.java
@@ -33,24 +33,27 @@ package oscar.oscarRx.pageUtil;
 
 import java.io.IOException;
 
+import java.util.HashMap;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.apache.struts.action.Action;
+import net.sf.json.JSONObject;
 import org.apache.struts.action.ActionForm;
 import org.apache.struts.action.ActionForward;
 import org.apache.struts.action.ActionMapping;
 
+import org.apache.struts.actions.DispatchAction;
 import oscar.oscarRx.data.RxPharmacyData;
+import oscar.oscarRx.data.RxPharmacyData.Pharmacy;
 
 /**
  *
  * @author  Jay Gallagher
  */
-public final class RxManagePharmacyAction extends Action {
+public final class RxManagePharmacyAction extends DispatchAction {
 
-    public ActionForward execute(ActionMapping mapping,
+    public ActionForward unspecified(ActionMapping mapping,
 				 ActionForm form,
 				 HttpServletRequest request,
 				 HttpServletResponse response)
@@ -71,7 +74,35 @@ public final class RxManagePharmacyAction extends Action {
            
        return mapping.findForward("success");     
     }
-   
+
+    public ActionForward getPharmacyInfo(ActionMapping mapping,
+				 ActionForm form,
+				 HttpServletRequest request,
+				 HttpServletResponse response)
+	throws IOException, ServletException {
+        String pharmacyId=request.getParameter("pharmacyId");
+        System.out.println("pharmacyId="+pharmacyId);
+        if(pharmacyId==null) return null;
+        RxPharmacyData pharmacyData = new RxPharmacyData();
+        Pharmacy pharmacy=pharmacyData.getPharmacy(pharmacyId);
+        HashMap hm=new HashMap();
+       if(pharmacy!=null){
+           hm.put("address", pharmacy.address);
+            hm.put("city", pharmacy.city);
+            hm.put("email", pharmacy.email);
+            hm.put("fax", pharmacy.fax);
+            hm.put("name", pharmacy.name);
+            hm.put("phone1", pharmacy.phone1);
+            hm.put("phone2", pharmacy.phone2);
+            hm.put("postalCode", pharmacy.postalCode);
+            hm.put("province", pharmacy.province);
+            hm.put("notes", pharmacy.notes);
+            System.out.println("in getPharmacyInfo,hm="+hm);
+            JSONObject jsonObject = JSONObject.fromObject(hm);
+            response.getOutputStream().write(jsonObject.toString().getBytes());
+       }
+        return null;
+    }
    /** Creates a new instance of RxManagePharmacyAction */
    public RxManagePharmacyAction() {
    }

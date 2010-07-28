cf5dcafef9c02e6112ac09350d8c607dcb74c7d6
annie_zhou91 <annie_zhou91>
2010-07-28 00:07:59 +0000

    *** empty log message ***

--- database/mysql/caisi/updates/patch-2010-06-08.sql
+++ database/mysql/caisi/updates/patch-2010-06-08.sql
@@ -1,3 +1,4 @@
 alter table secRole modify role_name varchar(60) NOT NULL default '';
 delete from program_provider where role_id is null;
 update secRole set role_name="counsellor" where role_name="Counsellor";
+alter table OcanStaffForm modify startDate date;

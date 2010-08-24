099df897469b529684cb45f8230d51f6ac675e4b
jaygallagher <jaygallagher>
2010-08-24 19:57:16 +0000

    id: 3029125 Added  to handle ADTA09 event Used in Health Card Swipe Oscar App Interface

---
+++ web/WEB-INF/classes/src/org/oscarehr/common/hl7/v2/oscar_to_oscar/DataTypeUtils.java
@@ -0,0 +1,344 @@
+package org.oscarehr.common.hl7.v2.oscar_to_oscar;
+
+import java.io.UnsupportedEncodingException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.GregorianCalendar;
+
+import org.apache.commons.codec.binary.Base64;
+import org.apache.commons.lang.StringUtils;
+import org.apache.log4j.Logger;
+import org.oscarehr.common.Gender;
+import org.oscarehr.common.model.Demographic;
+//import org.oscarehr.common.model.ProfessionalSpecialist;
+import org.oscarehr.common.model.Provider;
+import org.oscarehr.util.MiscUtils;
+
+import ca.uhn.hl7v2.HL7Exception;
+import ca.uhn.hl7v2.model.DataTypeException;
+import ca.uhn.hl7v2.model.v25.datatype.CX;
+import ca.uhn.hl7v2.model.v25.datatype.DT;
+import ca.uhn.hl7v2.model.v25.datatype.DTM;
+import ca.uhn.hl7v2.model.v25.datatype.FT;
+import ca.uhn.hl7v2.model.v25.datatype.PLN;
+import ca.uhn.hl7v2.model.v25.datatype.XAD;
+import ca.uhn.hl7v2.model.v25.datatype.XCN;
+import ca.uhn.hl7v2.model.v25.datatype.XPN;
+import ca.uhn.hl7v2.model.v25.datatype.XTN;
+import ca.uhn.hl7v2.model.v25.segment.MSH;
+import ca.uhn.hl7v2.model.v25.segment.NTE;
+import ca.uhn.hl7v2.model.v25.segment.PID;
+import ca.uhn.hl7v2.model.v25.segment.PRD;
+import ca.uhn.hl7v2.model.v25.segment.ROL;
+import ca.uhn.hl7v2.model.v25.segment.SFT;
+
+public final class DataTypeUtils {
+	private static final Logger logger = MiscUtils.getLogger();
+
+	public static final String HL7_VERSION_ID = "2.6";
+	public static final int NTE_COMMENT_MAX_SIZE = 65536;
+	public static final String ACTION_ROLE_SENDER = "SENDER";
+	public static final String ACTION_ROLE_RECEIVER = "RECEIVER";
+	private static final Base64 base64 = new Base64();
+
+	/**
+	 * Don't access this formatter directly, use the getAsFormattedString method, it provides synchronisation
+	 */
+	private static final SimpleDateFormat dateTimeFormatter = new SimpleDateFormat("yyyyMMddHHmmss");
+
+	private DataTypeUtils() {
+		// not meant to be instantiated by anyone, it's a util class
+	}
+
+	public static String encodeToBase64String(byte[] b) throws UnsupportedEncodingException {
+		return (new String(base64.encode(b), MiscUtils.ENCODING));
+	}
+
+	public static byte[] decodeBase64(String s) throws UnsupportedEncodingException {
+		return (base64.decode(s.getBytes(MiscUtils.ENCODING)));
+	}
+
+	public static String encodeToBase64String(String s) throws UnsupportedEncodingException {
+		return (new String(base64.encode(s.getBytes(MiscUtils.ENCODING)), MiscUtils.ENCODING));
+	}
+
+	public static String decodeBase64StoString(String s) throws UnsupportedEncodingException {
+		return (new String(base64.decode(s.getBytes(MiscUtils.ENCODING)), MiscUtils.ENCODING));
+	}
+
+	public static String getAsHl7FormattedString(Date date) {
+		synchronized (dateTimeFormatter) {
+			return (dateTimeFormatter.format(date));
+		}
+	}
+
+	private static GregorianCalendar getCalendarFromDT(DT dt) throws DataTypeException {
+
+		// hl7/hapi returns 0 for no date
+		if (dt.getYear() == 0 || dt.getMonth() == 0 || dt.getDay() == 0) return (null);
+
+		GregorianCalendar cal = new GregorianCalendar();
+		// zero out fields we don't use
+		cal.setTimeInMillis(0);
+		cal.set(GregorianCalendar.YEAR, dt.getYear());
+		cal.set(GregorianCalendar.MONTH, dt.getMonth() - 1);
+		cal.set(GregorianCalendar.DAY_OF_MONTH, dt.getDay());
+
+		// force materialisation of values
+		cal.getTimeInMillis();
+
+		return (cal);
+	}
+
+	public static GregorianCalendar getCalendarFromDTM(DTM dtm) throws DataTypeException {
+
+		// hl7/hapi returns 0 for no date
+		if (dtm.getYear() == 0 || dtm.getMonth() == 0 || dtm.getDay() == 0) return (null);
+
+		GregorianCalendar cal = new GregorianCalendar();
+		// zero out fields we don't use
+		cal.setTimeInMillis(0);
+		cal.set(GregorianCalendar.YEAR, dtm.getYear());
+		cal.set(GregorianCalendar.MONTH, dtm.getMonth() - 1);
+		cal.set(GregorianCalendar.DAY_OF_MONTH, dtm.getDay());
+		cal.set(GregorianCalendar.HOUR_OF_DAY, dtm.getHour());
+		cal.set(GregorianCalendar.MINUTE, dtm.getMinute());
+		cal.set(GregorianCalendar.SECOND, dtm.getSecond());
+
+		// force materialisation of values
+		cal.getTimeInMillis();
+
+		return (cal);
+	}
+
+
+
+	/**
+	 * @param msh
+	 * @param dateOfMessage
+	 * @param facilityName facility.getName();
+	 * @param messageCode i.e. "REF"
+	 * @param triggerEvent i.e. "I12"
+	 * @param messageStructure i.e. "REF_I12"
+	 * @param hl7VersionId is the version of hl7 in use, i.e. "2.6"
+	 */
+	public static void fillMsh(MSH msh, Date dateOfMessage, String facilityName, String messageCode, String triggerEvent, String messageStructure, String hl7VersionId) throws DataTypeException {
+		msh.getFieldSeparator().setValue("|");
+		msh.getEncodingCharacters().setValue("^~\\&");
+		msh.getVersionID().getVersionID().setValue(hl7VersionId);
+
+		//msh.getDateTimeOfMessage().setValue(getAsHl7FormattedString(dateOfMessage));
+
+		msh.getSendingApplication().getNamespaceID().setValue("OSCAR");
+
+		msh.getSendingFacility().getNamespaceID().setValue(facilityName);
+
+		// message code "REF", event "I12", structure "REF I12"
+		msh.getMessageType().getMessageCode().setValue(messageCode);
+		msh.getMessageType().getTriggerEvent().setValue(triggerEvent);
+		msh.getMessageType().getMessageStructure().setValue(messageStructure);
+	}
+
+	/**
+	 * @param sft
+	 * @param version major version if available
+	 * @param build build date or build number if available
+	 */
+	public static void fillSft(SFT sft, String version, String build) throws DataTypeException {
+		sft.getSoftwareVendorOrganization().getOrganizationName().setValue("OSCARMcMaster");
+		sft.getSoftwareCertifiedVersionOrReleaseNumber().setValue(version);
+		sft.getSoftwareProductName().setValue("OSCAR");
+		sft.getSoftwareBinaryID().setValue(build);
+	}
+
+
+
+	/**
+	 * This method returns a non-persisted, detached demographic model object.
+	 * 
+	 * @throws HL7Exception
+	 */
+	public static Demographic parsePid(PID pid) throws HL7Exception {
+		Demographic demographic = new Demographic();
+
+		XAD xad = pid.getPatientAddress(0);
+		demographic.setAddress(StringUtils.trimToNull(xad.getStreetAddress().getStreetOrMailingAddress().getValue()));
+		demographic.setCity(StringUtils.trimToNull(xad.getCity().getValue()));
+		demographic.setProvince(StringUtils.trimToNull(xad.getStateOrProvince().getValue()));
+		demographic.setPostal(StringUtils.trimToNull(xad.getZipOrPostalCode().getValue()));
+
+
+
+
+		GregorianCalendar birthDate = DataTypeUtils.getCalendarFromDTM(pid.getDateTimeOfBirth().getTime());
+		demographic.setBirthDay(birthDate);
+
+		CX cx = pid.getPatientIdentifierList(0);
+		// health card string, excluding version code
+		demographic.setHin(StringUtils.trimToNull(cx.getIDNumber().getValue()));
+		// blank for everyone but ontario use version code
+		//  demographic.setVer(StringUtils.trimToNull(cx.getIdentifierCheckDigit().getValue()));
+		// province
+		demographic.setHcType(StringUtils.trimToNull(cx.getAssigningJurisdiction().getIdentifier().getValue()));
+		// valid
+		GregorianCalendar tempCalendar = DataTypeUtils.getCalendarFromDT(cx.getEffectiveDate());
+		if (tempCalendar != null) demographic.setEffDate(tempCalendar.getTime());
+		// expire
+		tempCalendar = DataTypeUtils.getCalendarFromDT(cx.getExpirationDate());
+		if (tempCalendar != null) demographic.setHcRenewDate(tempCalendar.getTime());
+
+		XPN xpn = pid.getPatientName(0);
+		demographic.setLastName(StringUtils.trimToNull(xpn.getFamilyName().getSurname().getValue()));
+		demographic.setFirstName(StringUtils.trimToNull(xpn.getGivenName().getValue()));
+
+		XTN phone = pid.getPhoneNumberHome(0);
+		demographic.setPhone(StringUtils.trimToNull(phone.getUnformattedTelephoneNumber().getValue()));
+
+		Gender gender = getOscarGenderFromHl7Gender(pid.getAdministrativeSex().getValue());
+		if (gender != null) demographic.setSex(gender.name());
+
+		return (demographic);
+	}
+
+	/**
+	 * Given an oscar gender, this will return an hl7 gender.
+	 */
+	public static String getHl7GenderFromOscarGender(String oscarGender) {
+		Gender gender = null;
+
+		try {
+			gender = Gender.valueOf(oscarGender);
+		} catch (NullPointerException e) {
+			// this is okay, means there's no gender
+		} catch (Exception e) {
+			// this is not okay...
+			logger.error("Missed gender or dirty data in database. demographic.sex=" + oscarGender);
+		}
+
+		return (getHl7GenderFromOscarGender(gender));
+	}
+
+	/**
+	 * Given an oscar gender, this will return an hl7 gender.
+	 */
+	public static String getHl7GenderFromOscarGender(Gender oscarGender) {
+		if (oscarGender == null) return ("U");
+
+		if (Gender.M == oscarGender) return ("M");
+		else if (Gender.F == oscarGender) return ("F");
+		else if (Gender.O == oscarGender) return ("O");
+		else if (Gender.T == oscarGender) return ("A");
+		else if (Gender.U == oscarGender) return ("N");
+		else {
+			logger.error("Missed gender or dirty data in database. demographic.sex=" + oscarGender);
+			return ("U");
+		}
+	}
+
+	/**
+	 * Given an hl7 gender, this will return an oscarGender
+	 */
+	public static Gender getOscarGenderFromHl7Gender(String hl7Gender) {
+		if (hl7Gender == null) return (null);
+
+		hl7Gender = hl7Gender.toUpperCase();
+
+		if ("M".equals(hl7Gender)) return (Gender.M);
+		else if ("F".equals(hl7Gender)) return (Gender.F);
+		else if ("O".equals(hl7Gender)) return (Gender.O);
+		else if ("A".equals(hl7Gender)) return (Gender.T);
+		else if ("N".equals(hl7Gender)) return (Gender.U);
+		else if ("U".equals(hl7Gender)) return (null);
+		else throw (new IllegalArgumentException("Missed gender : " + hl7Gender));
+	}
+
+	/**
+	 * @param nte
+	 * @param subject should be a short string denoting what's in the comment data, i.e. "REASON_FOR_REFERRAL" or "ALLERGIES", max length is 250
+	 * @param fileName should be the file name if applicable, can be null if it didn't come from a file.
+	 * @param data and binary data, a String must pass in bytes too as it needs to be base64 encoded for \n and \r's
+	 * @throws HL7Exception
+	 * @throws UnsupportedEncodingException
+	 */
+	public static void fillNte(NTE nte, String subject, String fileName, byte[] data) throws HL7Exception, UnsupportedEncodingException {
+
+		nte.getCommentType().getText().setValue(subject);
+		if (fileName != null) nte.getCommentType().getNameOfCodingSystem().setValue(fileName);
+
+		String stringData = encodeToBase64String(data);
+		int dataLength = stringData.length();
+		int chunks = dataLength / DataTypeUtils.NTE_COMMENT_MAX_SIZE;
+		if (dataLength % DataTypeUtils.NTE_COMMENT_MAX_SIZE != 0) chunks++;
+		logger.debug("Breaking Observation Data (" + dataLength + ") into chunks:" + chunks);
+
+		for (int i = 0; i < chunks; i++) {
+			FT commentPortion = nte.getComment(i);
+
+			int startIndex = i * DataTypeUtils.NTE_COMMENT_MAX_SIZE;
+			int endIndex = Math.min(dataLength, startIndex + DataTypeUtils.NTE_COMMENT_MAX_SIZE);
+
+			commentPortion.setValue(stringData.substring(startIndex, endIndex));
+		}
+	}
+
+	public static byte[] getNteCommentsAsSingleDecodedByteArray(NTE nte) throws UnsupportedEncodingException {
+		FT[] fts = nte.getComment();
+
+		StringBuilder sb = new StringBuilder();
+		for (int i = 0; i < fts.length; i++)
+			sb.append(fts[i].getValue());
+
+		return (decodeBase64(sb.toString()));
+	}
+
+	/**
+	 * @param rol
+	 * @param provider
+	 * @param actionRole the role of the given provider with regards to this communcations. There is HL7 table 0443. We will also add DataTypeUtils.ACTION_ROLE_* as roles so we can send and receive arbitrary data under arbitrary conditions.
+	 * @throws HL7Exception
+	 */
+	public static void fillRol(ROL rol, Provider provider, String actionRole) throws HL7Exception {
+		rol.getActionCode().setValue("unused");
+		rol.getRoleROL().getIdentifier().setValue(actionRole);
+
+		XCN xcn = rol.getRolePerson(0);
+		xcn.getIDNumber().setValue(provider.getProviderNo());
+		xcn.getFamilyName().getSurname().setValue(provider.getLastName());
+		xcn.getGivenName().setValue(provider.getFirstName());
+		//xcn.getPrefixEgDR().setValue(provider.getTitle());
+
+		XAD xad = rol.getOfficeHomeAddressBirthplace(0);
+		xad.getStreetAddress().getStreetOrMailingAddress().setValue(StringUtils.trimToNull(provider.getAddress()));
+		xad.getAddressType().setValue("O");
+
+		XTN xtn = rol.getPhone(0);
+		xtn.getUnformattedTelephoneNumber().setValue(provider.getPhone());
+		//xtn.getCommunicationAddress().setValue(provider.getEmail());
+	}
+
+	/**
+	 * The provider returned is just a detached/unmanaged Provider object which may not represent an entry in the db, it is used as a data structure only.
+	 * 
+	 * @throws HL7Exception
+	 */
+	public static Provider parseRolAsProvider(ROL rol) throws HL7Exception {
+		Provider provider = new Provider();
+
+		XCN xcn = rol.getRolePerson(0);
+		provider.setProviderNo(StringUtils.trimToNull(xcn.getIDNumber().getValue()));
+		provider.setLastName(StringUtils.trimToNull(xcn.getFamilyName().getSurname().getValue()));
+		provider.setFirstName(StringUtils.trimToNull(xcn.getGivenName().getValue()));
+		//provider.setTitle(StringUtils.trimToNull(xcn.getPrefixEgDR().getValue()));
+
+		XAD xad = rol.getOfficeHomeAddressBirthplace(0);
+		provider.setAddress(StringUtils.trimToNull(xad.getStreetAddress().getStreetOrMailingAddress().getValue()));
+
+		XTN xtn = rol.getPhone(0);
+		provider.setPhone(StringUtils.trimToNull(xtn.getUnformattedTelephoneNumber().getValue()));
+		//provider.setEmail(StringUtils.trimToNull(xtn.getCommunicationAddress().getValue()));
+
+		return (provider);
+	}
+
+
+}
---
+++ web/WEB-INF/classes/src/oscar/oscarLab/ca/all/upload/handlers/OscarToOscarHl7V2/AdtA09Handler.java
@@ -0,0 +1,165 @@
+package oscar.oscarLab.ca.all.upload.handlers.OscarToOscarHl7V2;
+
+import java.sql.SQLException;
+import java.util.GregorianCalendar;
+import java.util.List;
+
+import org.apache.commons.lang.builder.ReflectionToStringBuilder;
+import org.apache.log4j.Logger;
+import org.oscarehr.common.dao.DemographicDao;
+import org.oscarehr.common.hl7.v2.oscar_to_oscar.DataTypeUtils;
+import org.oscarehr.common.model.Demographic;
+import org.oscarehr.common.model.OscarAppointment;
+import org.oscarehr.util.MiscUtils;
+import org.oscarehr.util.SpringUtils;
+
+import oscar.OscarProperties;
+import oscar.appt.AppointmentDao;
+import oscar.appt.status.model.AppointmentStatus;
+import oscar.dao.ProviderDao;
+import ca.uhn.hl7v2.HL7Exception;
+import ca.uhn.hl7v2.model.v25.message.ADT_A09;
+import ca.uhn.hl7v2.model.v25.segment.PV1;
+
+public final class AdtA09Handler {
+	private static Logger logger = MiscUtils.getLogger();
+
+	private static final String WAITING_ROOM = "WAITING_ROOM";
+	private static final String PATIENT_CLASS = "P";
+	private static AppointmentDao appointmentDao = (AppointmentDao) SpringUtils.getBean("appointmentDao");
+	private static DemographicDao demographicDao = (DemographicDao) SpringUtils.getBean("demographicDao");
+	private static int checkInLateAllowance=Integer.parseInt(OscarProperties.getInstance().getProperty(AdtA09Handler.class.getSimpleName()+".CHECK_IN_LATE_ALLOWANCE"));
+	private static int checkInEarlyAllowance=Integer.parseInt(OscarProperties.getInstance().getProperty(AdtA09Handler.class.getSimpleName()+".CHECK_IN_EARLY_ALLOWANCE"));	
+
+	public static void handle(ADT_A09 message) throws HL7Exception, SQLException {
+		// algorithm
+		// ----------
+		// unparse the hl7 message so we know who's checking in and make sure it's a check in
+		// look at appointments today for anyone with matching demographic info
+		// flip appointment status to H
+
+		// the 2 relavent segments
+		// PID|1||hhhhhhhhhh^^^^^^199704^199903^BC||last_name^first_name^^^^^L||19750607
+		// PV1||P|||||||||^WAITING_ROOM
+
+		checkPv1(message.getPV1());
+		Demographic demographic = DataTypeUtils.parsePid(message.getPID());
+
+		// look for the patient four hours ago and four hours from now
+		GregorianCalendar startTime = new GregorianCalendar();
+		startTime.add(GregorianCalendar.HOUR_OF_DAY, -checkInLateAllowance);
+		GregorianCalendar endTime = new GregorianCalendar();
+		// add 24 because it's atthe start of the day and it's exclusive of that day
+		endTime.add(GregorianCalendar.HOUR_OF_DAY, 24+checkInEarlyAllowance);
+
+		// so this only sorts out the day ranges i.e. we could have just done a select from today but this way we bridge 
+		// people checking in at 11:50pm for an appointment at 1:00am.
+		List<OscarAppointment> appointments = appointmentDao.findByDateRange(startTime.getTime(), endTime.getTime());
+		logger.debug("Qualifying appointments found : "+appointments.size());
+		
+		switchMatchingAppointment(demographic, appointments);
+	}
+
+	private static void switchMatchingAppointment(Demographic demographic, List<OscarAppointment> appointments) throws SQLException {
+	    // look through all appointments for matching demographic
+		// set the here flag on matching
+		// of not match throw exception.
+
+		for (OscarAppointment appointment : appointments) {
+			logger.debug("checking appointment : "+appointment.getAppointment_no());
+			
+			if ("H".equals(appointment.getStatus())) continue;
+			else if ("N".equals(appointment.getStatus())) continue;
+			else if ("C".equals(appointment.getStatus())) continue;
+			else if ("B".equals(appointment.getStatus())) continue;
+			
+			if (demographicMatches(appointment, demographic)) {				
+				switchAppointmentStatus(appointment);
+				return;
+			}
+		}
+
+		throw (new IllegalStateException("Some one checking in who has no appointment."));
+    }
+
+	/**
+	 * Check to make sure the PV1 is a check in as expected.
+	 */
+	private static void checkPv1(PV1 pv1) {
+		String patientClass = pv1.getPatientClass().getValue();
+		if (!PATIENT_CLASS.equals(patientClass)) throw (new UnsupportedOperationException("PV1 doesn't match expectations : patientClass=" + patientClass));
+
+		String room = pv1.getTemporaryLocation().getRoom().getValue();
+		if (!WAITING_ROOM.equals(room)) throw (new UnsupportedOperationException("PV1 doesn't match expectations : room=" + room));
+	}
+
+	private static boolean demographicMatches(OscarAppointment appointment, Demographic demographic) {
+		Demographic appointmentDemographic = demographicDao.getDemographicById(appointment.getDemographic_no());
+		return (demographicMatches(appointmentDemographic, demographic));
+	}
+
+	private static boolean demographicMatches(Demographic appointmentDemographic, Demographic demographic) {
+		// the expectation is that the demographic should have at least
+		// lastname, firstname, health number, health province, (gender/birthday are not ubiquitously available)
+		// there's a chance we may need to relax the birthday requirement to only need birth year/month as some or all BC cards may have no birthdays on them.
+		// because of name truncation, we will match only first and last initials of the names.
+		
+		if (logger.isDebugEnabled()) {
+			logger.debug("Checking demographic : " + ReflectionToStringBuilder.toString(demographic));
+			logger.debug("Against appointmentDemographic : " + ReflectionToStringBuilder.toString(appointmentDemographic));
+		}
+
+		if (demographic.getLastName() == null || demographic.getFirstName() == null || demographic.getHin() == null || demographic.getHcType() == null) {
+			logger.debug("fail : demographic has null data");
+			return (false);
+		}
+
+		char firstLetter=demographic.getLastName().toLowerCase().charAt(0);
+		if (firstLetter!=appointmentDemographic.getLastName().toLowerCase().charAt(0)) {
+			logger.debug("fail : last name");
+			return (false);
+		}
+
+		firstLetter=demographic.getFirstName().toLowerCase().charAt(0);
+		if (firstLetter!=appointmentDemographic.getFirstName().toLowerCase().charAt(0)) {
+			logger.debug("fail : first name");
+			return (false);
+		}
+		
+		if (!demographic.getHin().equals(appointmentDemographic.getHin())) {
+			logger.debug("fail : hin");
+			return (false);
+		}
+		
+		if (!demographic.getHcType().equalsIgnoreCase(appointmentDemographic.getHcType())) {
+			logger.debug("fail : hc type");
+			return (false);
+		}
+
+		// bc has no birthday
+		if (demographic.getBirthDay()!=null && !demographic.getBirthDay().equals(appointmentDemographic.getBirthDay())) {
+			logger.debug("fail : birthday");
+			return (false);
+		}
+		
+		// BC has no gender
+		if (demographic.getSex()!=null && !demographic.getSex().equalsIgnoreCase(appointmentDemographic.getSex())) {
+			logger.debug("fail : gender");
+			return (false);
+		}
+		
+		// for people in ontario, if there's a hc version, it needs to match too.
+		if (demographic.getVer() != null && !demographic.getVer().equalsIgnoreCase(appointmentDemographic.getVer())) {
+			logger.debug("fail : hc ver");
+			return (false);
+		}
+		
+		logger.debug("successful match");
+		return (true);
+	}
+
+	private static void switchAppointmentStatus(OscarAppointment appointment) throws SQLException {
+		ProviderDao.updateAppointmentStatus(appointment.getAppointment_no(), AppointmentStatus.APPOINTMENT_STATUS_HERE);
+	}
+
+}

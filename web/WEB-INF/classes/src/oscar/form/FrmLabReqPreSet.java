1b1a23cf4b1c5e82c915f793d1a1f38421f9d690
jaygallagher <jaygallagher>
2010-10-07 21:13:53 +0000

    ID: 3083289 Add logic to verfiy that a null values is not added to the list backported

--- web/WEB-INF/classes/src/oscar/form/FrmLabReqPreSet.java
+++ web/WEB-INF/classes/src/oscar/form/FrmLabReqPreSet.java
@@ -35,11 +35,25 @@ public class FrmLabReqPreSet {
                 }
                 else if( LabType.equals("AnteNatal") ) {
                     AnteNatalSet(props);
+                }else if (LabType.equals("DM")){
+                    DMSet(props);
                 }
 
                 return props;
         }
         
+        static void DMSet(Properties props){
+            props.setProperty("b_glucose","checked");
+            props.setProperty("b_glucose_fasting","checked");
+            props.setProperty("b_hba1c","checked");
+            props.setProperty("b_creatinine","checked");
+            props.setProperty("b_uricAcid","checked");
+            props.setProperty("b_alt","checked");
+            props.setProperty("b_alkPhosphatase","checked");
+            props.setProperty("b_lipidAssessment","checked");
+            props.setProperty("aci","\n Fast for 12 hours");
+        }
+
         static void AnteNatalSet(Properties props) {
             props.setProperty("b_urinalysis","checked");
             props.setProperty("h_cbc", "checked");

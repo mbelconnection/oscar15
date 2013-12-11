185a195e32e9e891624f332f35b35f5582e79156
Marc Dumontier <marc@mdumontier.com>
2013-12-11 00:53:02 -0500

    979 main navigation bar and more UI skeleton
    -added loading of schedule.json from the schedule controller

--- src/main/webapp/web/js/scheduleController.js
+++ src/main/webapp/web/js/scheduleController.js
@@ -1,2 +1,18 @@
 oscarApp.controller('ScheduleCtrl', function ($scope,$http) {
+	//load static models from web to inject 
+	$http({
+	    url: 'json/schedule.json',
+	    dataType: 'json',
+	    method: 'GET',
+	    headers: {
+	        "Content-Type": "application/json"
+	    }
+
+	}).success(function(response){
+		$scope.title = response.title;
+	   
+	}).error(function(error){
+	    $scope.error = error;
+	});	
+	
 });

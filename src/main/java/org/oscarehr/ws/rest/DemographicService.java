a892c1e733625ecdf82709cc621a253bf0d739f9
Jason Gallagher <jaygallagher@gmail.com>
2014-06-17 16:11:22 -0400

    Changed to produce and consume json
    Change-Id: I304a59787fb26e651364fe487d0341d18944d2d8

--- src/main/java/org/oscarehr/ws/rest/DemographicService.java
+++ src/main/java/org/oscarehr/ws/rest/DemographicService.java
@@ -23,12 +23,14 @@
  */
 package org.oscarehr.ws.rest;
 
+import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 
 import org.oscarehr.common.model.Demographic;
@@ -101,6 +103,7 @@ public class DemographicService extends AbstractServiceImpl {
 	 */
 	@GET
 	@Path("/{dataId}")
+	@Produces("application/json")
 	public DemographicTo1 getDemographicData(@PathParam("dataId") Integer id) {		
 		Demographic demo = demographicManager.getDemographic(id);
 		if (demo == null) {
@@ -120,6 +123,8 @@ public class DemographicService extends AbstractServiceImpl {
 	 * 		Returns the saved demographic data
 	 */
 	@POST
+	@Produces("application/json")
+	@Consumes("application/json")
 	public DemographicTo1 createDemographicData(DemographicTo1 data) {
 		Demographic demographic = demoConverter.getAsDomainObject(data);
 		demographicManager.createDemographic(demographic);
